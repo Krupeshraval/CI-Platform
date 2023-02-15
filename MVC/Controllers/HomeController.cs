5ac3497777dbce61f617b2ae4623ad0faf49a96f
Krupesh <krupeshraval43@gmail.com>
2023-02-15 13:54:08 +0530

    upgraded files to version .net6

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -1,11 +1,6 @@
 ﻿using CI_Project.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CI_Project.Controllers
 {
@@ -27,17 +22,14 @@ namespace CI_Project.Controllers
         {
             return View();
         }
-
         public IActionResult ForgotPassword()
         {
             return View();
         }
-
-        public IActionResult ReSetPassword()
+        public IActionResult ResetPassword()
         {
             return View();
         }
-
         public IActionResult Registration()
         {
             return View();
